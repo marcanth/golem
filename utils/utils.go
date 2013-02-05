c155843572c032e8e280adb06958dfd73645b432
João Carlos <joaocarlos422@gmail.com>
2013-02-05 11:48:16 -0200

    Adding ignore files function

--- utils/utils.go
+++ utils/utils.go
@@ -1,17 +1,57 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
 
-func GetPaths(s string) []string {
-	var paths []string = make([]string, 0)
+const (
+	CONFIG_FILE = "config.json"
+	SEPARATOR   = string(os.PathSeparator)
+)
+
+func GetPaths(s string) (paths List) {
+	var ap, ip = getAllPaths(s), getIgnoredPaths(s)
+	for k, v := range ap {
+		v = strings.Join(strings.Split(v, SEPARATOR)[1:], SEPARATOR)
+		if ip.SearchBy(v) == false {
+			paths = append(paths, ap[k])
+		}
+	}
+	return
+}
+
+func getAllPaths(s string) (paths List) {
+	s = path.Clean(s)
 	filepath.Walk(s, func(path string, info os.FileInfo, err error) error {
 		paths = append(paths, path)
 		return err
 	})
-	return paths
+	return
+}
+
+func getIgnoredPaths(src string) List {
+	src = path.Clean(src)
+	src = path.Join(src, CONFIG_FILE)
+
+	f, err := os.Open(src)
+	if err != nil {
+		panic(err)
+	}
+	d := json.NewDecoder(f)
+
+	var file File
+	d.Decode(&file)
+
+	return file.Ignored
+}
+
+type File struct {
+	Ignored []string
 }
 
 type List []string
