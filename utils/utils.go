e2bc3c03bb7212b9f1df72de1841e88c9f106a7b
João Carlos <joaocarlos422@gmail.com>
2013-02-07 13:53:33 -0200

    Improving argument parse and paths getter

--- utils/utils.go
+++ utils/utils.go
@@ -2,19 +2,45 @@ package utils
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
+	"time"
 )
 
 const (
 	CONFIG_FILE = "config.json"
 	SEPARATOR   = string(os.PathSeparator)
+	DATE_FORMAT = "20060102150405"
 )
 
+type Config struct {
+	Ignored List
+	Destination string
+}
+
+func getConfig(s string) (c Config, err error) {
+	_s := path.Clean(s)
+	s = path.Join(_s, CONFIG_FILE)
+	f, err := os.Open(s)
+	if err == nil {
+		d := json.NewDecoder(f)
+		d.Decode(&c)
+		c.Ignored = append(c.Ignored, CONFIG_FILE)
+		for k, v := range c.Ignored {
+			c.Ignored[k] = path.Join(_s, v)
+		}
+	}
+
+	return
+}
+
+type List []string
+
 func GetPaths(s string) (paths List) {
-	var ignoredPaths, allPaths List
+	var allPaths List
 
 	s = path.Clean(s)
 	filepath.Walk(s, func(path string, info os.FileInfo, err error) error {
@@ -22,30 +48,20 @@ func GetPaths(s string) (paths List) {
 		return err
 	})
 
-	s = path.Join(s, CONFIG_FILE)
-	f, err := os.Open(s)
-	if err == nil {
-		d := json.NewDecoder(f)
-		var file File
-		d.Decode(&file)
-		ignoredPaths = file.Ignored
-		ignoredPaths = append(ignoredPaths, CONFIG_FILE)
+	config, err := getConfig(s)
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println(config.Ignored)
 
 	for k, v := range allPaths {
-		v = strings.Join(strings.Split(v, SEPARATOR)[1:], SEPARATOR)
-		if ignoredPaths.SearchBy(v) == false {
+		fmt.Println(v)
+		if config.Ignored.SearchBy(v) == false {
 			paths = append(paths, allPaths[k])
 		}
 	}
 	return
 }
-
-type File struct {
-	Ignored []string
-}
-
-type List []string
 
 func (l List) SearchBy(n interface{}) bool {
 	switch t := n.(type) {
@@ -64,4 +80,26 @@ func (l List) SearchBy(n interface{}) bool {
 	}
 
 	return false
+}
+
+func ParseArgs(a []string) (args map[string]string) {
+	args = make(map[string]string)
+	switch len(a) {
+	case 2:
+		args["source"] = a[1]
+		if c, err := getConfig(args["source"]); err == nil {
+			args["dest"] = c.Destination
+		} else {
+			panic(err)
+		}
+	case 3:
+		args["source"] = a[1]
+		args["dest"] = a[2]
+	}
+
+	var zip string
+	flag.StringVar(&zip, "o", time.Now().Format("DATE_FORMAT"), "Enter with the zip name here")
+	args["zip"] = zip
+
+	return
 }
